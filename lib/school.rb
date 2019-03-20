3ee4749f82312aae57ade000504146589a8b0e07
Sachin Mahajan <sachinm78@gmail.com>
2019-03-20 17:49:46 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -11,25 +11,18 @@ class School
   def add_student(student_name, grade)
     roster[grade] ||= []
     roster[grade] << student_name
-  end 
-  
-  def grade(grade_level)
-    roster.detect do |x, y| 
-      if x == grade_level
-        return y 
-      end 
-    end 
   end
   
-  def sort 
-    new_hash = {}
-    roster.each do |x, y| 
-    new_hash[x] = y.sort 
-  end 
-  new_hash
-end 
-
+  def grade(grade)
+    roster[grade]
+  end
   
+  def sort
+    sorted = {}
+    roster.each do |grade, name|
+      sorted[grade] = name.sort
+    end
+    sorted
+  end
 end
-
 
